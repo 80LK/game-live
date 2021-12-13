dfc51ac288c3b12f24c25a458e6ef07001a0776e
80LK <80LK@users.noreply.github.com>
2021-12-14 01:22:09 +0300

    write game

---
+++ live.js
@@ -0,0 +1,166 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Cell = /** @class */ (function () {
+    function Cell() {
+    }
+    return Cell;
+}());
+function count(arr, predicate) {
+    if (predicate === void 0) { predicate = function () { return true; }; }
+    return arr.reduce(function (r, el, index, arr) {
+        if (predicate(el, index, arr))
+            r++;
+        return r;
+    }, 0);
+}
+var LiveCell = /** @class */ (function (_super) {
+    __extends(LiveCell, _super);
+    function LiveCell() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    LiveCell.prototype.calculate = function (cells) {
+        var lives = count(cells, function (cell) { return cell instanceof LiveCell; });
+        if (lives < 2 || lives > 3)
+            return new DieCell();
+        return this;
+    };
+    LiveCell.prototype.toString = function () {
+        return "1";
+    };
+    return LiveCell;
+}(Cell));
+var DieCell = /** @class */ (function (_super) {
+    __extends(DieCell, _super);
+    function DieCell() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    DieCell.prototype.calculate = function (cells) {
+        var lives = count(cells, function (cell) { return cell instanceof LiveCell; });
+        if (lives == 3)
+            return new LiveCell();
+        return this;
+    };
+    DieCell.prototype.toString = function () {
+        return "0";
+    };
+    return DieCell;
+}(Cell));
+var Game = /** @class */ (function () {
+    function Game(width, height, grid) {
+        if (height === void 0) { height = 0; }
+        if (grid === void 0) { grid = []; }
+        this.interval = null;
+        this._ticks = 0;
+        if (typeof width != "number") {
+            grid = width;
+            width = grid[0].length;
+            height = grid.length;
+        }
+        this.width = width;
+        this.height = height;
+        this._grid = [];
+        for (var y = 0; y < this.height; y++) {
+            var row = [];
+            for (var x = 0; x < this.width; x++) {
+                if (grid[y]) {
+                    var cell = grid[y][x];
+                    row.push(cell ? ((cell instanceof Cell) ? cell : new LiveCell()) : new DieCell());
+                }
+                else {
+                    row.push(new DieCell());
+                }
+            }
+            this._grid.push(row);
+        }
+    }
+    Object.defineProperty(Game.prototype, "grid", {
+        get: function () {
+            return this._grid.map(function (row) { return row.map(function (cell) { return cell; }); });
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Game.prototype, "ticks", {
+        get: function () {
+            return this._ticks;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Game.prototype.get = function (x, y) {
+        if (x < 0)
+            x = this.width + x;
+        else if (x >= this.width)
+            x = x - this.width;
+        if (y < 0)
+            y = this.height + y;
+        else if (y >= this.height)
+            y = y - this.height;
+        return this._grid[y][x];
+    };
+    Game.prototype.calculate = function () {
+        var _this = this;
+        this._grid = this._grid.map(function (row, y) { return row.map(function (cell, x) {
+            var cells = [];
+            for (var _x = -1; _x <= 1; _x++)
+                for (var _y = -1; _y <= 1; _y++)
+                    if (_x != 0 || _y != 0)
+                        cells.push(_this.get(x + _x, y + _y));
+            return cell.calculate(cells);
+        }); });
+    };
+    Game.prototype.tick = function () {
+        this._ticks++;
+        this.calculate();
+        this.render();
+    };
+    Game.prototype.start = function (timeout) {
+        if (timeout === void 0) { timeout = 1000; }
+        this.render();
+        this.interval = setInterval(this.tick.bind(this), timeout);
+    };
+    Game.prototype.stop = function () {
+        if (this.interval)
+            clearInterval(this.interval);
+    };
+    return Game;
+}());
+var ConsoleGame = /** @class */ (function (_super) {
+    __extends(ConsoleGame, _super);
+    function ConsoleGame() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ConsoleGame.prototype.render = function () {
+        console.clear();
+        console.log("Tick:", this.ticks);
+        for (var y = 0; y < this.height; y++) {
+            var row = "";
+            for (var x = 0; x < this.width; x++) {
+                row += this.grid[y][x] + " ";
+            }
+            console.log(row);
+        }
+    };
+    return ConsoleGame;
+}(Game));
+var game = new ConsoleGame(10, 10, [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+]);
---
+++ live.ts
@@ -0,0 +1,143 @@
+abstract class Cell {
+	public abstract calculate(neighbours: Cell[]): Cell;
+}
+
+function count<T>(arr: T[], predicate: (value: T, index: number, array: T[]) => boolean = () => true): number {
+	return arr.reduce<number>((r, el, index, arr) => {
+		if (predicate(el, index, arr))
+			r++;
+		return r;
+	}, 0);
+}
+
+class LiveCell extends Cell {
+	calculate(cells: Cell[]): Cell {
+		const lives = count(cells, cell => cell instanceof LiveCell);
+		if (lives < 2 || lives > 3)
+			return new DieCell();
+
+		return this;
+	}
+
+	public toString(): string {
+		return "1";
+	}
+}
+
+class DieCell extends Cell {
+	calculate(cells: Cell[]): Cell {
+		const lives = count(cells, cell => cell instanceof LiveCell);
+		if (lives == 3)
+			return new LiveCell();
+
+		return this;
+	}
+
+	public toString(): string {
+		return "0";
+	}
+}
+
+type Grid = (number | boolean | Cell)[][];
+abstract class Game {
+	constructor(width: number, height: number, grid: Grid);
+	constructor(grid: Grid);
+	constructor(width: Grid | number, height: number = 0, grid: Grid = []) {
+		if (typeof width != "number") {
+			grid = width;
+			width = grid[0].length;
+			height = grid.length;
+		}
+
+		this.width = width;
+		this.height = height;
+		this._grid = [];
+		for (let y = 0; y < this.height; y++) {
+			const row: Cell[] = [];
+
+			for (let x = 0; x < this.width; x++) {
+				if (grid[y]) {
+					const cell = grid[y][x];
+					row.push(cell ? ((cell instanceof Cell) ? cell : new LiveCell()) : new DieCell());
+				} else {
+					row.push(new DieCell());
+				}
+			}
+			this._grid.push(row);
+		}
+	}
+
+	protected readonly width: number;
+	protected readonly height: number;
+	private _grid: Cell[][];
+	protected get grid(): Readonly<Cell[][]> {
+		return this._grid.map(row => row.map(cell => cell));
+	}
+	private interval: number | null = null;
+
+	private _ticks: number = 0;
+	protected get ticks() {
+		return this._ticks;
+	}
+
+	protected get(x: number, y: number) {
+		if (x < 0) x = this.width + x;
+		else if (x >= this.width) x = x - this.width;
+		if (y < 0) y = this.height + y;
+		else if (y >= this.height) y = y - this.height;
+
+		return this._grid[y][x];
+	}
+
+	protected calculate() {
+		this._grid = this._grid.map((row, y) => row.map((cell, x) => {
+			const cells: Cell[] = [];
+			for (let _x = -1; _x <= 1; _x++)
+				for (let _y = -1; _y <= 1; _y++)
+					if (_x != 0 || _y != 0)
+						cells.push(this.get(x + _x, y + _y));
+
+			return cell.calculate(cells);
+		}));
+	}
+
+	private tick() {
+		this._ticks++;
+		this.calculate();
+		this.render();
+	}
+
+	protected abstract render(): void;
+
+	public start(timeout: number = 1000) {
+		this.render();
+		this.interval = setInterval(this.tick.bind(this), timeout);
+	}
+	public stop() {
+		if (this.interval)
+			clearInterval(this.interval);
+	}
+
+}
+
+class ConsoleGame extends Game {
+	public render() {
+		console.clear();
+		console.log("Tick:", this.ticks);
+		for (let y = 0; y < this.height; y++) {
+			let row = "";
+			for (let x = 0; x < this.width; x++) {
+				row += this.grid[y][x] + " ";
+			}
+			console.log(row);
+		}
+	}
+}
+
+var game = new ConsoleGame(10, 10, [
+	[0, 0, 0, 0, 0],
+	[0, 0, 1, 0, 0],
+	[0, 0, 0, 1, 0],
+	[0, 1, 1, 1, 0],
+	[0, 0, 0, 0, 0],
+]);
